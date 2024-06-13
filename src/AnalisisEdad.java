53cdccb66ee4aac2448bfda78fe5b90a41fb8db0
jesusususususu <jesusbenitogonzalez1234@gmail.com>
2024-06-13 10:05:35 +0200

    añadir clases

--- src/AnalisisEdad.java
+++ src/AnalisisEdad.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AnalisisEdad {
 
+Scanner sc = new Scanner (System.in);
+	
+	private ArrayList<Integer> edad;
+	
+	String frase;	
+	public AnalisisEdad (ArrayList<Integer> edad) {
+		this.edad=edad;
+	}
+	
+	public String AnalisisEdad (ArrayList<Integer> edad) {
+	int sumar=0;
+	int contar=0;
+		for(int i=0; i<edad.size(); i++) {
+			if(edad.get(i)%2==0) {
+			     sumar=sumar+i;
+			}else if (edad.get(i)%2==1) {
+				contar++;
+			}
+		}
+	}
+	
+	frase== "Suma de pares :"+sumar+"recuento de impares :"+ contar;
+	return frase;
+		
 }
--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
 
 public class PrincipalApp {
 
+public static void main(String[]args) {
+		
+		ArrayList<Integer>edad=new ArrayList<Integer>();
+		edad.add(1);
+		edad.add(11);
+		edad.add(10);
+		edad.add(21);
+		edad.add(15);
+		edad.add(14);
+		edad.add(5);
+		edad.add(8);
+		
+		AnalisisEdad analisis = new AnalisisEdad(edad);
+		System.out(analisis.comprobar(edad));
+	}
 }
